62ccd6d95d75081d6592c2e15299fa94f25d2727
jdittert <jondittert@gmail.com>
2023-04-01 14:24:45 -0400

    Move home page creation to home.js

---
+++ src/home.js
@@ -0,0 +1,56 @@
+function home() {
+    // Create initial main
+    const element = document.createElement('main');
+    element.setAttribute('id', 'main');
+
+    // Create top section
+    const mainWrapper = document.createElement('div');
+    mainWrapper.classList.add('main-wrapper');
+    
+    // Card with cookie
+    const mainImage = document.createElement('div');
+    mainImage.classList.add('card', 'image-card');
+    const cookieImage = document.createElement('img');
+    cookieImage.src = 'swindle-cookie.jpg';
+    cookieImage.classList.add('main-image');
+    cookieImage.setAttribute('alt', 'Cookie');
+    mainImage.appendChild(cookieImage);
+    mainWrapper.appendChild(mainImage);
+
+    // Card with description
+    const description = document.createElement('div');
+    description.classList.add('card', 'description');
+    const descText = document.createElement('div');
+    descText.classList.add('cardtext');
+    const descTop = document.createElement('p');
+    descTop.innerText = 'We only sell one thing - delicious chocolate chip cookies!';
+    const descBot = document.createElement('p');
+    descBot.innerText = 'Despite the different prices listed on the menu, we assure you that all cookies are identical. '
+    +'There is absolutely no difference between any of the cookies at any of the price points.';
+    descText.appendChild(descTop);
+    descText.appendChild(descBot);
+    description.appendChild(descText);
+    mainWrapper.appendChild(description);
+
+    // Create bottom section    
+    const testCard = document.createElement('div');
+    testCard.classList.add('card-long', 'testimonial');
+    const testimonial = document.createElement('div');
+    testimonial.classList.add('cardtext');
+    testimonial.innerText = '"I\'ve tried all of the cookies. I really think I can taste the difference between them." -- Totally Real Customer';
+    testCard.appendChild(testimonial);
+    mainWrapper.appendChild(testCard);
+
+    // Append sections to main
+    element.appendChild(mainWrapper);    
+
+    return element;
+}
+
+function loadHome() {    
+    const main = document.getElementById('main');
+    main.textContent = '';
+    main.appendChild(home());
+}
+
+export default loadHome;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import loadHome from './home';
 
 function header() {    
     // Create initial header
@@ -28,49 +29,8 @@ function header() {
 }
 
 function main() {
-    // Create initial main
     const element = document.createElement('main');
-
-    // Create top section
-    const mainWrapper = document.createElement('div');
-    mainWrapper.classList.add('main-wrapper');
-    
-    // Card with cookie
-    const mainImage = document.createElement('div');
-    mainImage.classList.add('card', 'image-card');
-    const cookieImage = document.createElement('img');
-    cookieImage.src = 'swindle-cookie.jpg';
-    cookieImage.classList.add('main-image');
-    cookieImage.setAttribute('alt', 'Cookie');
-    mainImage.appendChild(cookieImage);
-    mainWrapper.appendChild(mainImage);
-
-    // Card with description
-    const description = document.createElement('div');
-    description.classList.add('card', 'description');
-    const descText = document.createElement('div');
-    descText.classList.add('cardtext');
-    const descTop = document.createElement('p');
-    descTop.innerText = 'We only sell one thing - delicious chocolate chip cookies!';
-    const descBot = document.createElement('p');
-    descBot.innerText = 'Despite the different prices listed on the menu, we assure you that all cookies are identical. '
-    +'There is absolutely no difference between any of the cookies at any of the price points.';
-    descText.appendChild(descTop);
-    descText.appendChild(descBot);
-    description.appendChild(descText);
-    mainWrapper.appendChild(description);
-
-    // Create bottom section    
-    const testCard = document.createElement('div');
-    testCard.classList.add('card-long', 'testimonial');
-    const testimonial = document.createElement('div');
-    testimonial.classList.add('cardtext');
-    testimonial.innerText = '"I\'ve tried all of the cookies. I really think I can taste the difference between them." -- Totally Real Customer';
-    testCard.appendChild(testimonial);
-    mainWrapper.appendChild(testCard);
-
-    // Append sections to main
-    element.appendChild(mainWrapper);    
+    element.setAttribute('id', 'main');
 
     return element;
 }
@@ -93,6 +53,8 @@ function footer() {
     return element;
 }
 
-document.getElementById('content').appendChild(header());
-document.getElementById('content').appendChild(main());
-document.getElementById('content').appendChild(footer());
+const content = document.getElementById('content');
+content.appendChild(header());
+content.appendChild(main());
+loadHome();
+content.appendChild(footer());
